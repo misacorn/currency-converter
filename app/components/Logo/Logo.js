94669645db9e745191d7249177262549393927ef
Misa Corn <dieuhuongngo@gmail.com>
2019-03-04 14:18:14 +0200

    adding Logo

---
+++ app/components/Logo/Logo.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text, Image } from "react-native";
+
+const Logo = () => {
+  <View>
+    <Image>
+      <Image source={require("./image/logo.png")} />
+    </Image>
+    <Text>Currency Converter</Text>
+  </View>;
+};
+
+export default Logo;
