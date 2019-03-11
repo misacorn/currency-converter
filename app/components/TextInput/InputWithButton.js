74a9fda58e2acc70812ecac1024d3e74cae43116
Misa <dieuhuongngo@gmail.com>
2019-03-11 16:01:06 +0200

    templates for input component

---
+++ app/components/TextInput/InputWithButton.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text, TextInput, TouchableHighlight } from "react-native";
+
+import styles from "./styles";
+
+const InputWithButton = () => (
+  <View>
+    <TextInput />
+  </View>
+);
+
+export default InputWithButton;
--- app/screens/Home.js
+++ app/screens/Home.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { Component } from "react";
 import { StatusBar } from "react-native";
 
 import Container from "../components/Container/Container";
 import Logo from "../components/Logo/Logo";
+import InputWithButton from "../components/TextInput/InputWithButton";
 
-export default () => (
-  <Container>
-    <StatusBar translucent={false} barStyle="light-content" />
-    <Logo />
-  </Container>
-);
+class Home extends Component {
+  render() {
+    return (
+      <Container>
+        <StatusBar translucent={false} barStyle="light-content" />
+        <Logo />
+        <InputWithButton />
+        <InputWithButton editable={false} />
+      </Container>
+    );
+  }
+}
+
+export default Home;
