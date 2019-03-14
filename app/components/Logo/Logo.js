4f5519d982600367435cb3b8a45579bc7cd0ff3e
Misa Corn <dieuhuongngo@gmail.com>
2019-03-14 13:19:06 +0200

    remove/show the keyboard

--- app/components/Logo/Logo.js
+++ app/components/Logo/Logo.js
@@ -1,16 +1,44 @@
-import React from "react";
-import { View, Text, Image } from "react-native";
+import React, { Component } from "react";
+import { View, Text, Image, Keyboard } from "react-native";
 import styles from "./styles";
 
-const Logo = () => (
-  <View style={styles.container}>
-    <Image
-      style={styles.containerImg}
-      // resizeModel="contain"
-      source={require("./image/logo.png")}
-    />
-    <Text style={styles.text}>Currency Converter</Text>
-  </View>
-);
+class Logo extends Component {
+  componentDidMount() {
+    this.keyboardShowListener = Keyboard.addListener(
+      "keyboardWillShow",
+      this.keyboardShow
+    );
+    this.keyboardHideListerner = Keyboard.addListener(
+      "keyboardWillHide",
+      this.keyboardHide
+    );
+  }
+
+  componentWillUnmount() {
+    this.keyboardShowListener.remove();
+    this.keyboardHideListerner.remove();
+  }
+
+  keyboardShow = () => {
+    console.log("keyboard did show");
+  };
+
+  keyboardHide = () => {
+    console.log("keyboard did hide");
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Image
+          style={styles.containerImg}
+          // resizeModel="contain"
+          source={require("./image/logo.png")}
+        />
+        <Text style={styles.text}>Currency Converter</Text>
+      </View>
+    );
+  }
+}
 
 export default Logo;
--- app/components/Logo/styles.js
+++ app/components/Logo/styles.js
@@ -4,14 +4,19 @@ import EStyleSheet from "react-native-extended-stylesheet";
 const imageWidth = Dimensions.get("window").width / 2;
 
 export default EStyleSheet.create({
+  $largeImageSize: imageWidth,
+  $smallImageSize: imageWidth / 2,
   container: {
     alignItems: "center"
   },
   containerImg: {
     alignItems: "center",
     justifyContent: "center",
-    width: imageWidth,
-    height: imageWidth
+    width: "$largeImageSize",
+    height: "$largeImageSize"
+  },
+  logo: {
+    width: "$largeImageSize"
   },
   text: {
     fontWeight: "600",
