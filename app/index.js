e434bc952b1f4e2ad12c1d31f8579e9103cc3546
Misa Corn <dieuhuongngo@gmail.com>
2019-03-15 02:27:23 +0200

    import hardcoded data

--- app/index.js
+++ app/index.js
@@ -1,15 +1,18 @@
 import React from "react";
 import EStyleSheet from "react-native-extended-stylesheet";
 
-import Home from "./screens/Home";
+// import Home from "./screens/Home";
+import CurrencyList from "./screens/CurrencyList";
 
 EStyleSheet.build({
   $primaryColor: "#a537fd",
   $white: "#fff",
   $border: "#E2E2E2",
   $inputText: "#797979",
-  $disabled: "#F0F0F0"
+  $disabled: "#F0F0F0",
+  $darkText: "#343434"
+
   // $outline: 1
 });
 
-export default () => <Home />;
+export default () => <CurrencyList />;
